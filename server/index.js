a1d0935bfa453ff39ab4daf1c732067ca17a919e
Peter Klavins <paklavins@gmail.com>
2019-08-28 15:46:43 -0600

    Build?

--- server/index.js
+++ server/index.js
@@ -61,9 +61,16 @@ app.post('/api/cancel', (req, res) => {
 
 });
 
-app.get('/', express.static(__dirname + 'public'));
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
 
-
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => console.log(`Listening on localhost:${PORT}`));
